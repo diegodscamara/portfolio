4ba18bb668acd7d26f90173fa3b92c4de4df14ef
Diego Camara <diegodscamara@gmail.com>
2023-03-02 19:06:47 -0500

    fixing building bugs

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
             <h4>Tambasa - E-commerce</h4>
             <span>Tambasa is a Brazilian wholesale and retail store that specializes in construction materials, tools, and home appliances. I helped them create a new e-commerce store that was integrated with their existing Oracle system. The website was intended to go live for their Initial Public Offering (IPO), and I successfully met the deadline. To achieve this, I used a range of technologies, including React, JavaScript, HTML, CSS, Less, Adobe XD, and REST.
               With this project, I was able to deliver a modern and user-friendly e-commerce platform with an intuitive UI that allowed customers to quickly find and purchase the products they needed. The site was optimized for performance and scalability, ensuring that it could handle high volumes of traffic and transactions. Additionally, I implemented a range of features that enhanced the customer experience, such as a responsive design that worked seamlessly across desktop and mobile devices, and an easy-to-use checkout process that streamlined the purchase flow.
-              Overall, the project was a success and helped to increase Tambasa's online sales and customer engagement.</span>
+              Overall, the project was a success and helped to increase Tambasa&apos;s online sales and customer engagement.</span>
             <Link href="https://tambasa.com/">Check it out</Link>
           </li>
           <li className="text-lg mb-4">
