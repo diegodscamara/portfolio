6215ca75e3b181bd411da20fc2ce8db074b02477
Diego Camara <diegodscamara@gmail.com>
2023-03-26 13:23:16 -0400

    🎨 style(Hero.tsx): remove unused linkStyles variable ✨ feat(Hero.tsx): replace anchor tag with Button component for better accessibility and add variant prop to Button component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,10 @@
+import Button from '../button';
 import Link from 'next/link';
 import React from 'react';
 import { ScrollDownIcon } from 'public/scroll-down-icon';
 
 const Hero = () => {
   const sectionStyles = 'container mx-auto px-8 h-screen justify-center text-center flex flex-col items-center'
-  const linkStyles = 'bg-primary text-black font-bold font-sans h-10 flex items-center justify-center rounded-sm w-44 hover:opacity-80 mb-20';
   const textStyles = 'text-4xl md:text-5xl lg:text-6xl text-primary font-sans font-normal mb-4';
   const titleStyles = 'text-3xl md:text-4xl lg:text-tagline text-white font-sans font-bold mb-4';
   const descStyles = 'text-card mb-4';
@@ -16,8 +16,17 @@ const Hero = () => {
       <span className={descStyles}>
         Crafting exceptional user experiences as a front end developer with a passion for UI/UX design
       </span>
-      <Link href={'#Contact'} title={'Get in touch'} aria-label={'Get in touch'} className={linkStyles}>Get in touch</Link>
-      <Link href={'#About'} title={'Scroll down'} aria-label={'Scroll down'} className="animate-bounce">
+
+      <Button 
+        attributes={{
+          link: '#Contact',
+          title: 'Get in touch'
+        }}
+        variant={'filled'}>
+          Get in touch
+      </Button>
+      
+      <Link href={'#About'} title={'Scroll down'} aria-label={'Scroll down'} className={'animate-bounce mt-20'}>
         <ScrollDownIcon />
       </Link>
     </section>
