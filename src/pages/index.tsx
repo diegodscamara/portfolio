fec12435c9de2a3021e06c2f3e28065b003fb0a8
Diego Camara <diegodscamara@gmail.com>
2023-03-26 16:14:42 -0400

    📝 chore(index.tsx): add meta description to the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,7 @@ const Home = () => {
     <>
       <Head>
         <title>Diego Câmara - Front End Developer</title>
+        <meta name="description" content="As a Front End Developer, I have a passion for creating user-friendly interfaces using technologies such as ReactJS, NextJS, JavaScript, TypeScript, HTML, CSS, and my design skills extend beyond coding, as I’m proficient in Figma to bring my visions to life. I integrate frontend systems with content management systems and have experience with both GraphQL and REST APIs. With expertise in Scrum, Kanban, Agile, and code versioning tools." />
       </Head>
       <Header />
       <main>
