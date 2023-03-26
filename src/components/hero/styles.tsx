4d798cd1bd21c6f9141a489ad53acabf36f5c9f8
Diego Camara <diegodscamara@gmail.com>
2023-03-26 19:43:23 -0400

    🎨 style(hero): adjust title and subtitle font sizes and colors, add text-align center to container

--- src/components/hero/styles.tsx
+++ src/components/hero/styles.tsx
@@ -8,56 +8,19 @@ export const Container = styled.section`
   height: 100vh;
   margin: 0 auto;
   padding: 0 2rem;
+  text-align: center;
 `;
 
-export const Title = styled.h1`
-  font-size: 2.25rem;
-  line-height: 2.75rem;
-  font-weight: 400;
+export const Title = styled.h2`
   margin-bottom: 1rem;
-
-  @media (min-width: 768px) {
-    font-size: 2.5rem;
-    line-height: 3rem;
-  }
-
-  @media (min-width: 1024px) {
-    font-size: 3rem;
-    line-height: 3.5rem;
-  }
-
-  @media (min-width: 1280px) {
-    font-size: 3.75rem;
-    line-height: 4.25rem;
-  }
+  color: var(--primary-color);
 `;
 
-export const Subtitle = styled.h2`
-  font-size: 1.875rem;
-  line-height: 2.25rem;
-  font-weight: 700;
-  color: #ffffff;
+export const Subtitle = styled.h1`
   margin-bottom: 1rem;
-
-  @media (min-width: 768px) {
-    font-size: 2.25rem;
-    line-height: 2.75rem;
-  }
-
-  @media (min-width: 1024px) {
-    font-size: 2.5rem;
-    line-height: 3rem;
-  }
-
-  @media (min-width: 1280px) {
-    font-size: 3rem;
-    line-height: 3.5rem;
-  }
 `;
 
 export const Description = styled.span`
-  font-size: 1rem;
-  color: #4b5563;
   margin-bottom: 1rem;
 `;
 
