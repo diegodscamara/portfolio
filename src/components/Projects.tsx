dd3829b930607e3d860a76056783b568c255342b
Diego Camara <diegodscamara@gmail.com>
2023-03-27 19:20:30 -0400

    🎨 style(Projects.tsx): change Projects title from h1 to h3 tag

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,7 @@ const Projects = () => {
   return (
     <div>
       <section className="container mx-auto px-8 py-36" id='Projects'>
-        <h1 className="text-4xl font-bold mb-4">Projects</h1>
+        <h3 className="text-4xl font-bold mb-4">Projects</h3>
         <ul>
           <li className="text-lg mb-4">
             <h4>Tambasa - E-commerce</h4>
