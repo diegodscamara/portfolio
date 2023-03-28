c177cf75dfe2f89050786321c4552874dcd546d5
Diego Camara <diegodscamara@gmail.com>
2023-03-28 19:29:58 -0400

    🎨 style(expertise): adjust styles for better centering and spacing 🔥 chore(expertise): remove unused styles and add missing newline at end of file

--- src/components/expertise/styles.tsx
+++ src/components/expertise/styles.tsx
@@ -3,29 +3,38 @@ import styled from 'styled-components';
 export const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  justify-content: center;
+  align-items: center;
   padding: 16px;
-  gap: 16px;
+  gap: 24px;
   width: 100%;
-  border: 2px solid #E5E7EB;
-  border-radius: 0.375rem;
+  border: 2px solid #494949;
+  border-radius: 4px;
 `;
 
-export const CardTitle = styled.h3`
-`;
+export const CardTitle = styled.h4``;
 
-export const CardDescription = styled.span``;
+export const CardDescription = styled.span`
+  text-align: center;
+  color: #B5B5B5;
+`;
 
 export const ExpertiseContainer = styled.section`
-  margin: 0 auto;
-  padding: 8rem 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 32px;
 `;
 
-export const ExpertiseTitle = styled.h3`
-  text-align: center;
-`;
+export const ExpertiseTitle = styled.h3``;
 
 export const ExpertiseCardWrapper = styled.div`
   display: flex;
-  gap: 1rem;
+  flex-direction: row;
+  gap: 16px;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
