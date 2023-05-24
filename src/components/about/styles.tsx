e8875beafc41f1afec6486d6dc701a65fa69d94c
Diego Camara <diegodscamara@gmail.com>
2023-05-23 20:59:12 -0400

    🎨 style(about/index.tsx): refactor About component to use new styles 🚀 chore(about/index.tsx): replace AboutIllustration with AboutImage The About component has been refactored to use new styles. The AboutIllustration component has been replaced with the AboutImage component, which is an SVG image. This change improves the performance of the component as SVG images are smaller in size and can be scaled without losing quality.
    🎨 style(about/styles.tsx): change AboutFlexContainer to Wrapper, change svg to img, add flex-direction and width properties
    🎨 style(about/styles.tsx): change AboutSectionTitle to Title, remove text-transform property
    🎨 style(about/styles.tsx): change AboutDescription to Description
    🎨 style(button/styles.tsx): add width property to ButtonStyles
    The changes in the about/styles.tsx file include renaming the AboutFlexContainer to Wrapper, changing the svg element to img, and adding the flex-direction and width properties. The AboutSectionTitle was renamed to Title and the text-transform property was removed. The AboutDescription was renamed to Description. In the button/styles.tsx file, the width property was added to the ButtonStyles. These changes improve the consistency and readability of the code.

--- src/components/about/styles.tsx
+++ src/components/about/styles.tsx
@@ -1,30 +1,20 @@
 import styled from 'styled-components';
 
-export const AboutContainer = styled.section``;
-
-export const AboutFlexContainer = styled.div`
+export const Wrapper = styled.section`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   justify-content: space-between;
   gap: 32px;
+  width: 100%;
 
-  @media (min-width: 1200px) {
-    flex-direction: row;
-    gap: 64px;
-  }
-`;
-
-export const IllustrationContainer = styled.div`
-  svg {
-    display: flex;
-    width: 660px;
+  img {
+    width: 100%;
+    height: 100%;
   }  
-  
+
   @media only screen and (max-width: 1024px) {
-    svg {
-      width: 100%;
-    }
+    flex-direction: column-reverse;
   }
 `;
 
@@ -45,34 +35,14 @@ export const Header = styled.div`
   gap: 1rem;
 `;
 
-export const AboutSectionTitle = styled.h3`
+export const Title = styled.h3`
   display: flex;
   align-items: center;
   gap: 1rem;
   font-size: var(--font-size-md);
-  text-transform: uppercase;
   color: var(--button-color);
 `;
 
-export const AboutDescription = styled.span`
+export const Description = styled.span`
   color: var(--text-color);
-`;
-
-export const AboutButtons = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 16px;
-  align-items: center;
-
-  @media only screen and (max-width: 600px) {
-    flex-direction: column;
-
-    a, button {
-      width: 100%;
-    }
-  }
-`;
-
-export const ToolTip = styled.span`
-  color: var(--light-gray);
 `;
