b1a70fe4f05db32db699ec145daec76201f7071e
Diego Câmara <dcamara@genetec.com>
2023-06-26 18:27:36 -0400

    folders update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "070c562a28adc062"; }
+/******/ 		__webpack_require__.h = function() { return "048cb683ebf03520"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
