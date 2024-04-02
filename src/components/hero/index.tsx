d6dc10eb2b6e570904613f76a0dc97d84ca7d077
Diego Câmara <dcamara@genetec.com>
2024-04-02 18:27:32 -0300

    Update profile image and dimensions in hero component

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,16 +1,16 @@
 import {
-	Column,
-	Container,
-	ScrollLink,
-	Subtitle,
-	Title,
-	Wrapper,
-} from './styles'
+  Column,
+  Container,
+  ScrollLink,
+  Subtitle,
+  Title,
+  Wrapper,
+} from "./styles";
 
-import { AudioContext } from '../../context/AudioContext'
-import { Button } from '../../components/button'
-import { PlayAudio } from '../play-audio'
-import { useContext } from 'react'
+import { AudioContext } from "../../context/AudioContext";
+import { Button } from "../../components/button";
+import { PlayAudio } from "../play-audio";
+import { useContext } from "react";
 
 /**
  * Renders the Hero component.
@@ -18,56 +18,58 @@ import { useContext } from 'react'
  * @return {JSX.Element} The rendered Hero component.
  */
 export function Hero(): JSX.Element {
-	const { audio } = useContext(AudioContext)
+  const { audio } = useContext(AudioContext);
 
-	return (
-		<Wrapper id='Home'>
-			<Container>
-				<Column>
-					<Title>
-						Hi, I&apos;m Diego Câmara
-						<br />
-						A Front End Developer
-						<br />
-						Based in Montreal
-					</Title>
-					<Subtitle>
-						I help businesses and companies reach
-						<br />
-						their goals by designing and developing
-						<br />
-						user-centric web applications
-					</Subtitle>
-					<Button variant='filled'>
-						<a href='#Contact' title='Get in touch'>
-							Get in touch
-						</a>
-					</Button>
-				</Column>
+  return (
+    <Wrapper id="Home">
+      <Container>
+        <Column>
+          <Title>
+            Hi, I&apos;m Diego Câmara
+            <br />
+            A Front End Developer
+            <br />
+            Based in Montreal
+          </Title>
+          <Subtitle>
+            I help businesses and companies reach
+            <br />
+            their goals by designing and developing
+            <br />
+            user-centric web applications
+          </Subtitle>
+          <Button variant="filled">
+            <a href="#Contact" title="Get in touch">
+              Get in touch
+            </a>
+          </Button>
+        </Column>
 
-				<Column>
-					<img
-						src='/images/avatar.svg'
-						alt='Diego Câmara - Front End Developer'
-						width={420}
-						height={420}
-					/>
-				</Column>
-			</Container>
+        <Column>
+          <img
+            src="/images/profile.jpeg"
+            alt="Diego Câmara - Front End Developer"
+            width={320}
+            height={220}
+            className="radius"
+          />
+        </Column>
+      </Container>
 
-			<ScrollLink
-				href='#About'
-				title='Scroll down'
-				onClick={() => {
-					audio === 'on' && PlayAudio({ file: '/sounds/click.wav' })
-				}}>
-				<img
-					src='/icons/scroll-down-icon.svg'
-					alt='Scroll down'
-					width={24}
-					height={24}
-				/>
-			</ScrollLink>
-		</Wrapper>
-	)
+      <ScrollLink
+        href="#About"
+        title="Scroll down"
+        onClick={() => {
+          audio === "on" && PlayAudio({ file: "/sounds/click.wav" });
+        }}
+      >
+        <img
+          src="/icons/scroll-down-icon.svg"
+          alt="Scroll down"
+          width={24}
+          height={24}
+        />
+      </ScrollLink>
+    </Wrapper>
+  );
 }
